66291acd62571b9f47c86fe362c3c3532b4e0bdf
Nik FP <nik@nikfp.com>
2026-06-11 14:55:32 -0600

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,29 +31,48 @@
 </script>
 
 <div class="flex flex-col items-center p-6">
-	<h1 class="text-2xl">Randomaker</h1>
+	<h1 class="my-4 text-2xl">Randomaker</h1>
 
 	<form onsubmit={addToList} class="flex w-full max-w-md">
 		<label for="list-item" class="sr-only">List Input</label>
 		<input
 			id="list-item"
-			bind:value={inputValue}
+			name="list-item"
 			type="text"
 			placeholder="input a list item here"
-			class="border"
-			class:border-gray-400={!errorMessages}
-			class:border-red-500={!!errorMessages}
+			class={[
+				'flex-1 rounded-l-md border bg-white px-4 py-2.5',
+				'text-sm text-zinc-800 placeholder:text-zinc-400 focus:z-10 focus:border-blue-500',
+				'focus:ring-2 focus:ring-blue-500 focus:outline-none',
+				!errorMessages && 'border-zinc-400',
+				!!errorMessages && 'border-red-500'
+			]}
+			bind:value={inputValue}
 			bind:this={inputEl}
 		/>
-		{#if errorMessages}
-			{#each errorMessages as error, i (error.key)}
-				<p id={`error-message-${i}`} class="text-red-500">{error.message}</p>
-			{/each}
-		{/if}
-		<button type="submit" class="mt-4 rounded-md border border-gray-400 bg-gray-200 px-2"
-			>Add to List</button
+		<button
+			type="submit"
+			class={[
+				'rounded-r-md bg-blue-600 px-4 py-2.5',
+				'text-sm font-medium text-white ',
+				'hover:bg-blue-700 focus:ring-2 focus:outline-none',
+				'focus:ring-blue-500 focus:ring-offset-2'
+			]}>Add to List</button
 		>
 	</form>
+
+	{#if errorMessages}
+		{#each errorMessages as error, i (error.key)}
+			<p id={`error-message-${i}`} class="text-red-500">{error.message}</p>
+		{/each}
+	{/if}
+
+	<section class="rounted-lg border border-zinc-300 bg-white">
+		<h2 class="flex">
+			Existing list items:
+			<button>+</button>
+		</h2>
+	</section>
 
 	<div class="mt-4 w-fit border border-zinc-500 p-4">
 		{#each listStore.value as item, index (item.key)}
